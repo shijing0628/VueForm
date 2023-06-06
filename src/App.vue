67687bf7b205df4e58519a1c8f352b3cfae9da0f
Shijing Liu <shijing.liu@accenture.com>
2023-06-05 19:39:07 -0500

    add submit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Payment</h3>
-    <form novalidate>
+    <form novalidate @submit.prevent="onSave()">
       <div class="row">
         <div class="col-md-6">
           <div><strong>Shipping Information</strong></div>
@@ -12,6 +12,7 @@
               id="name"
               class="form-control"
               placeholder="your name"
+              v-model="payment.fullName"
             />
           </div>
           <div class="form-group">
@@ -21,6 +22,7 @@
               id="company"
               class="form-control"
               placeholder="company"
+              v-model="payment.company"
             />
           </div>
           <div class="form-group">
@@ -30,6 +32,7 @@
               id="Address1"
               class="form-control"
               placeholder="street address"
+              v-model="payment.address1"
             />
           </div>
           <div class="form-group">
@@ -39,6 +42,7 @@
               id="Address2"
               class="form-control"
               placeholder="street address"
+              v-model="payment.address2"
             />
           </div>
          <div class="form-row">
@@ -49,15 +53,17 @@
                 id="cityTown"
                 class="form-control"
                 placeholder="e.g. new York"
+                v-model="payment.cityTown"
               />
             </div>
              <div class="form-group col-md-3">
               <label for="stateProvince">State</label>
               <select
                 id="stateProvince"
-                class="form-control"   
+                class="form-control"
+                v-model="payment.stateProvince"   
               >
-              <option>CA</option>
+              <option v-for="s in states" :key="s.abbreviation" :value="s.abbreviation">{{s.name}}</option>
               </select>
             </div>
             <div class="form-group col-md-3">
@@ -67,6 +73,7 @@
                 id="postalCode"
                 class="form-control"
                 placeholder="e.g.10101"
+                v-model="payment.postalCode"  
               />
             </div>
          </div>
@@ -79,5 +86,29 @@
         </div>
       </div>
     </form>
+    <div><pre>{{payment}}</pre></div>
   </div>
 </template>
+
+<script>
+import {ref} from "vue";
+import states from "./lookup/states";
+
+export default {
+setup(){
+  const payment =ref ({
+    fullName:"Brenda",
+    postalCode:"12345"
+  });
+ function onSave(){
+  alert("hi hi");
+ }
+  return{
+    payment,
+    states,
+    onSave
+  };
+}
+}
+
+</script>
